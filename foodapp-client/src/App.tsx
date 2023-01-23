550a042b6bba871cb42d3ea3b0508603fe2284df
vladc <vlad.colceriu@yahoo.com>
2023-01-23 16:08:10 +0200

    Remove unit tests temporarily

--- foodapp-client/src/App.tsx
+++ foodapp-client/src/App.tsx
@@ -1,56 +1,26 @@
-import './App.scss';
-
-import HomePage from './components/home-page/home-page';
-import {
-  Routes,
-  Route,
-  Navigate
-} from "react-router-dom";
-import ResponsiveAppBar from './components/layout/responsive-app-bar/responsive-app-bar';
 import React from 'react';
-import SuppliersList from './components/suppliers-list/suppliers-list';
-import UserProfile from './components/user-profile/user-profile';
-import AdminPage from './components/admin-page/admin-page';
-import SupplierPage from './components/supplier-page/supplier-page';
-import AuthForm from './components/auth-form/auth-form';
-import { Container } from '@mui/material';
-import { useSelector } from 'react-redux';
-import { RootState } from './store';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
   return (
-    <React.Fragment>
-      <ResponsiveAppBar />
-      <Container maxWidth="lg">
-        <Routes>
-          <Route path="/login" element={<AuthForm />} />
-
-          {isLoggedIn &&
-            <Route path="/" element={<HomePage />} />}
-
-          {isLoggedIn &&
-            <Route path="suppliers" element={<SuppliersList />} >
-              <Route path=":supplierId" element={<SupplierPage />} />
-            </Route>}
-
-          {isLoggedIn &&
-            <Route path="user-profile" element={<UserProfile />} />}
-
-          {isLoggedIn &&
-            <Route path="admin-page" element={<AdminPage />} />}
-
-          <Route
-            path="*"
-            element={
-              <Navigate to="/login" replace />
-            }
-          />
-        </Routes>
-      </Container>
-
-    </React.Fragment>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+	<div>Salut sunt bogdanel de la bucuresti</div>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- foodapp-client/src/index.tsx
+++ foodapp-client/src/index.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { BrowserRouter } from 'react-router-dom';
-import store from './store';
-import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
